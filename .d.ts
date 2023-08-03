9fdba103e6ec97ccd2d91ee94d80b146511d3aec
charlielin123 <lingd123@gmail.com>
2023-08-03 12:09:20 +0800

    type加入.d.ts 日後需考慮改地放放置

--- .d.ts
+++ .d.ts
@@ -4,3 +4,31 @@ declare module '*.vue' {
   const componentOptions: ComponentOptions;
   export default componentOptions;
 }
+
+type Card = {
+  _id: string;
+  name: string;
+  content?: string;
+  dueDate?: Date;
+};
+type CardBox = {
+  _id: string;
+  name: string;
+  cards: Card[];
+  mission: Mission;
+};
+type mission = {
+  _id: string;
+  name: string;
+  owner: User;
+  editor?: User[];
+  cardBoxes?: CardBox[];
+};
+
+type User = {
+  _id: string;
+  email: string;
+  googleId?: string;
+  name?: string;
+  image?: string;
+};
