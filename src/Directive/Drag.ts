4966716a6b1abfac7efd78462429aef388d5915b
charlielin123 <lingd123@gmail.com>
2023-09-08 01:31:43 +0800

    修正拖拉功能放大後拖曳位置偏離問題

--- src/Directive/Drag.ts
+++ src/Directive/Drag.ts
@@ -13,8 +13,8 @@ export default (el: HTMLInputElement, bind: DirectiveBinding, vNode: VNode) => {
   let startTop = 0;
   let startLeft = 0;
 
-  let startHeight = el.clientHeight;
-  let startWidth = el.clientWidth;
+  let startHeight = vNode.props?.style?.height ?? el.offsetHeight + 'px';
+  let startWidth = vNode.props?.style?.width ?? el.offsetWidth + 'px';
   let dragBar = bind.value;
   let inner = bind.value?.inner;
   if (!(dragBar instanceof HTMLElement)) {
@@ -27,9 +27,11 @@ export default (el: HTMLInputElement, bind: DirectiveBinding, vNode: VNode) => {
   const maxWidth = inner?.clientWidth || window.innerWidth;
   const borderTop = inner?.offsetTop || 0;
   const borderLeft = inner?.offsetLeft || 0;
+  const temp: { top: number; left: number } = { top: 0, left: 0 };
+  let flag = true;
 
-  el.style.setProperty('top',(window.innerHeight-el.offsetHeight)/2+'px');
-  el.style.setProperty('left',(window.innerWidth-el.offsetWidth)/2+'px');
+  el.style.setProperty('top', (window.innerHeight - el.offsetHeight) / 2 + 'px');
+  el.style.setProperty('left', (window.innerWidth - el.offsetWidth) / 2 + 'px');
 
   let f11 = false;
   const initDrag = (event: MouseEvent) => {
@@ -40,13 +42,6 @@ export default (el: HTMLInputElement, bind: DirectiveBinding, vNode: VNode) => {
     startY = event.clientY;
     startTop = el.offsetTop;
     startLeft = el.offsetLeft;
-    if (f11 ) {
-      // startX = el.offsetLeft;
-      // startY = el.offsetTop;
-      startTop = event.clientY;
-      startLeft = event.clientX;
-    }
-
     document.addEventListener('mousemove', drag);
     document.addEventListener('mouseup', stopDrag);
   };
@@ -57,36 +52,44 @@ export default (el: HTMLInputElement, bind: DirectiveBinding, vNode: VNode) => {
    */
   const drag = (event: MouseEvent) => {
     if (!dragging) return;
-
+    // console.log(startTop, startLeft);
     const deltaX = event.clientX - startX;
     const deltaY = event.clientY - startY;
-    if (f11 && deltaY > 0) {
-      el.style.width = startWidth + 'px';
-      el.style.height = startHeight + 'px';
-      // startX = event.clientX;
-      // startY = event.clientY;
-      // startTop = event.clientY;
-      // startLeft = event.clientX;
+    if (f11 && deltaY > 10) {
+      el.style.width = startWidth;
+      el.style.height = startHeight;
+      const x = dragBar.offsetWidth / 2;
+      const y = dragBar.offsetHeight / 2;
+      startTop = event.clientY - y;
+      startLeft = event.clientX - x;
+      el.style.setProperty('top', temp.top + 'px');
+      el.style.setProperty('left', temp.left + 'px');
       f11 = false;
       return;
     }
 
     let top = startTop + deltaY;
     let left = startLeft + deltaX;
+
     if (top + 10 >= maxHeight - el.clientHeight) {
       top = maxHeight - el.clientHeight;
-    }
-    if (top <= 10) {
-      top = 0;
-      el.style.width = '100vw';
-      el.style.height = '100vh';
-      f11 = true;
     }
     if (left <= 10) {
       left = 0;
     }
     if (left + 10 > maxWidth - el.clientWidth) {
       left = maxWidth - el.clientWidth;
+    }
+    if (top <= 10) {
+      if (f11 == false) {
+        temp.top = el.offsetTop;
+        temp.left = el.offsetLeft;
+        f11 = true;
+      }
+      top = 0;
+      left = 0;
+      el.style.width = '100vw';
+      el.style.height = '100vh';
     }
 
     el.style.top = plusPx(top);
@@ -95,6 +98,7 @@ export default (el: HTMLInputElement, bind: DirectiveBinding, vNode: VNode) => {
 
   const stopDrag = () => {
     dragging = false;
+    console.log(temp);
     document.removeEventListener('mousemove', drag);
     document.removeEventListener('mouseup', stopDrag);
   };
